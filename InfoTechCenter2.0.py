551c4bf8b2ece15e3a9d96bf8ba7ec30f83b0d2f
Tucker mcIntyre <Tmcintyre0623@gmail.com>
2022-02-24 11:04:51 -0800

    Updated VRS Function with ELIF & ELSE statements - Stable

--- InfoTechCenter2.0.py
+++ InfoTechCenter2.0.py
@@ -14,5 +14,20 @@ def vehicleResponseSystem():
     if weatherAlert == "Icy":
         print("\nVRS has changed your Alarm 30 minutes earlier based on the NWS forecast of",weatherAlert,":(")
         print("VRS will only allow your vehicle to go 30mph")
+    elif weatherAlert == "Snowy":
+        print("\nVRS has changed your Alarm 15 minutes earlier based on the NWS forecast of", weatherAlert, ":(")
+        print("VRS will only allow your vehicle to go 15mph")
+    elif weatherAlert == "Raining":
+        print("\nVRS has changed your Alarm 5 minutes earlier based on the NWS forecast of", weatherAlert, ":(")
+        print("VRS will only allow your vehicle to go 60mph")
+    elif weatherAlert == "Windy":
+        print("\nVRS has changed your Alarm 5 minutes earlier based on the NWS forecast of", weatherAlert, ":(")
+        print("VRS will only allow your vehicle to go 70mph")
+    elif weatherAlert == "Foggy":
+        print("\nVRS has changed your Alarm 5 minutes earlier based on the NWS forecast of", weatherAlert, ":(")
+        print("VRS will only allow your vehicle to go 60mph")
+    else:
+        print("\nWeather is",weatherAlert, "Let's GOOOOOOOOOOOO")
+        print("VRS will alow your car to go 120MPH")
 
 vehicleResponseSystem()
